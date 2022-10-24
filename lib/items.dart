768fc382d3c85ec9bffe6432023f44d8f949e666
Onur A. Karakum <113251720+oakarakum@users.noreply.github.com>
2022-10-24 16:51:33 +0300

    develop oluşturuldu

---
+++ lib/items.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+
+class Items with ChangeNotifier {}
---
+++ lib/pages/applyjob_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ApplyJobScreen extends StatefulWidget {
+  const ApplyJobScreen({super.key});
+
+  @override
+  State<ApplyJobScreen> createState() => _ApplyJobScreenState();
+}
+
+class _ApplyJobScreenState extends State<ApplyJobScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
