62b06ba1fad99f6d687d6c114037fcf3c6493980
Onur A. Karakum <113251720+oakarakum@users.noreply.github.com>
2022-10-24 19:07:42 +0300

    items güncellendi

--- lib/items.dart
+++ lib/items.dart
@@ -1,5 +1,43 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class Items with ChangeNotifier {}
+class Items with ChangeNotifier {
+  List popularJobsList = [
+    {
+      "Photo": "assets/homepage_assets/altasian-logo.svg",
+      "CompanyName": "Altasian",
+      "job": "Product Designer",
+      "location": "Dublin, Ireland",
+    },
+    {
+      "Photo": "assets/homepage_assets/one pro.svg",
+      "CompanyName": "One pro",
+      "job": "Product Designer",
+      "location": "Miami, USA",
+    },
+    {
+      "Photo": "assets/homepage_assets/shopify.svg",
+      "CompanyName": "Shopify",
+      "job": "Data Analytics",
+      "location": "Ottawa, Ireland",
+    },
+    {
+      "Photo": "assets/homepage_assets/shopify.svg",
+      "CompanyName": "Shopify",
+      "job": "Data Analytics",
+      "location": "Ottawa, Ireland",
+    },
+  ];
+  List jobType = [
+    {
+      "jobtype": "Freelance",
+    },
+    {
+      "jobtype": "Part time",
+    },
+    {
+      "jobtype": "Remote",
+    },
+  ];
+}
