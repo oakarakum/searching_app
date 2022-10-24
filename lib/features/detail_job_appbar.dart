c96d24cee81676ea21de35e55d70a85f29185348
Onur A. Karakum <113251720+oakarakum@users.noreply.github.com>
2022-10-24 22:51:36 +0300

    güncelleme

--- lib/features/detail_job_appbar.dart
+++ lib/features/detail_job_appbar.dart
@@ -13,14 +13,10 @@ class _DetailjobAppbarState extends State<DetailjobAppbar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Color(0xff0F0C07),
-      leading: IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset("assets/homepage_assets/Arrow-Left.svg")),
-      actions: [
-        InkWell(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Color(0xff0F0C07),
+        leading: InkWell(
           onTap: () {
             Navigator.push(
                 context,
@@ -28,16 +24,30 @@ class _DetailjobAppbarState extends State<DetailjobAppbar> {
                   builder: (context) => HomePageScreen(),
                 ));
           },
-          child: Padding(
+          child: Image.asset("assets/homepage_assets/Arrow-Left.png"),
+        ),
+        actions: [
+          Padding(
             padding: const EdgeInsets.only(right: 10),
             child: Row(children: [
-              SvgPicture.asset("assets/homepage_assets/favourite.svg",color: Colors.white,),
+              SvgPicture.asset(
+                "assets/homepage_assets/favourite.svg",
+                color: Colors.white,
+              ),
               SizedBox(width: 10),
               SvgPicture.asset("assets/homepage_assets/Logout-Outline.svg")
             ]),
           ),
-        )
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
+/* InkWell(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePageScreen(),
+                ));
+          }, */
