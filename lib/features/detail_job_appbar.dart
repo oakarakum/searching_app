2063089b60213822ef8f7a9257203e93c149ff41
oakarakum <onur.ardakarakum@outlook.com>
2022-10-27 09:59:12 +0300

    appbardan scaffold silindi.

--- lib/features/detail_job_appbar.dart
+++ lib/features/detail_job_appbar.dart
@@ -12,34 +12,32 @@ class DetailjobAppbar extends StatefulWidget {
 class _DetailjobAppbarState extends State<DetailjobAppbar> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Color(0xff0F0C07),
-        leading: InkWell(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HomePageScreen(),
-                ));
-          },
-          child: Image.asset("assets/homepage_assets/Arrow-Left.png"),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: Row(children: [
-              SvgPicture.asset(
-                "assets/homepage_assets/favourite.svg",
-                color: Colors.white,
-              ),
-              SizedBox(width: 10),
-              SvgPicture.asset("assets/homepage_assets/Logout-Outline.svg")
-            ]),
-          ),
-        ],
+    return AppBar(
+      elevation: 1,
+      backgroundColor: Color(0xff0F0C07),
+      leading: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePageScreen(),
+              ));
+        },
+        child: Image.asset("assets/homepage_assets/Arrow-Left.png"),
       ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Row(children: [
+            SvgPicture.asset(
+              "assets/homepage_assets/favourite.svg",
+              color: Colors.white,
+            ),
+            SizedBox(width: 10),
+            SvgPicture.asset("assets/homepage_assets/Logout-Outline.svg")
+          ]),
+        ),
+      ],
     );
   }
 }
